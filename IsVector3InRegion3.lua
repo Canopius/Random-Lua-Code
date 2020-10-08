cea17018994203142e3eedae95a226b2c57569a9
Josh <65733500+Canopius@users.noreply.github.com>
2020-10-08 19:03:10 +0100

    Update IsVector3InRegion3.lua

--- IsVector3InRegion3.lua
+++ IsVector3InRegion3.lua
@@ -3,8 +3,8 @@
 local function IsV3InR3(V3, R3)
 	local R3CFrame = R3.CFrame
 	local R3Size = R3.Size
-	local R3Min = Vector3.new(R3CFrame.X - (R3.X * 0.5), R3CFrame.Y - (R3.Y * 0.5), R3CFrame.Z - (R3.Z * 0.5))
-	local R3Max = Vector3.new(R3CFrame.X + (R3.X * 0.5), R3CFrame.Y + (R3.Y * 0.5), R3CFrame.Z + (R3.Z * 0.5))
+	local R3Min = Vector3.new(R3CFrame.X - (R3.Size.X * 0.5), R3CFrame.Y - (R3.Size.Y * 0.5), R3CFrame.Z - (R3.Size.Z * 0.5))
+	local R3Max = Vector3.new(R3CFrame.X + (R3.Size.X * 0.5), R3CFrame.Y + (R3.Size.Y * 0.5), R3CFrame.Z + (R3.Size.Z * 0.5))
 	
 	if V3.X < R3Min.X or V3.X > R3Max.X or V3.Y < R3Min.Y or V3.Y > R3Max.Y or V3.Z < R3Min.Z or V3.Z > R3Max.Z then
 		return false
